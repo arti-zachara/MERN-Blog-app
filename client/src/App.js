f1a7380b71b185505447a84d506c6933edd6c2fd
arti Zachara <magdalena.j.zachara@gmail.com>
2019-07-24 11:48:19 +0200

    Add basic posts display do App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,28 @@
 import React from "react";
 
 class App extends React.Component {
-  render() {
-    return <div>Hello world!</div>;
+
+  state = {
+    posts: [],
   }
+
+  componentDidMount() {
+    fetch('http://localhost:8000/api/posts')
+      .then(res => res.json())
+      .then(res => {
+        this.setState({ posts: res });
+    });
+  }
+
+  render() {
+  return (
+    <div>
+      <ul>
+          {this.state.posts.map(post => <li key={post.id}>{post.title}</li>)}
+      </ul>
+    </div>
+  );
+}
 }
 
 export default App;
