ad3b4fb2f452d1b29c52755b6d28e6fc94771c8a
arti Zachara <magdalena.j.zachara@gmail.com>
2019-07-24 13:20:17 +0200

    Add router

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import "./styles/global.scss";
 
-const Root = () => <App />;
+const Root = () => (
+  <Router>
+    <App />
+  </Router>
+);
 
 ReactDOM.render(<Root />, document.getElementById("root"));
