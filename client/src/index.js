8ec0ff57bf9b2f5b9507282c86b722754fdf87df
arti Zachara <magdalena.j.zachara@gmail.com>
2019-07-25 17:23:36 +0200

    Add and import store.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+
 import App from "./App";
 import "./styles/global.scss";
+import store from "./redux/store";
 
 const Root = () => (
-  <Router>
-    <App />
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
 );
 
 ReactDOM.render(<Root />, document.getElementById("root"));
