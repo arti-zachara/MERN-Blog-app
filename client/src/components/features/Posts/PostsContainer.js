b900b4896ab2dc6675ae55570e14fc8bb9e81491
arti Zachara <magdalena.j.zachara@gmail.com>
2019-07-27 20:54:54 +0200

    Add conditions for Spinner and PostsList display to Posts

--- client/src/components/features/Posts/PostsContainer.js
+++ client/src/components/features/Posts/PostsContainer.js
@@ -2,13 +2,15 @@ import { connect } from "react-redux";
 import {
   getRequest,
   getPosts,
+  getPostsNumber,
   loadPostsRequest
 } from "../../../redux/PostsRedux";
 import Posts from "./Posts";
 
 const mapStateToProps = state => ({
   posts: getPosts(state),
-  request: getRequest(state)
+  request: getRequest(state),
+  postsNumber: getPostsNumber(state)
 });
 
 const mapDispatchToProps = dispatch => ({
