0c7fdbf653f40457bb19e2a84aeeb8c9989aec15
arti Zachara <magdalena.j.zachara@gmail.com>
2019-07-27 19:36:21 +0200

    Add Request Error hndling

--- client/src/redux/PostsRedux.js
+++ client/src/redux/PostsRedux.js
@@ -12,19 +12,23 @@ export const getPostsNumber = ({ posts }) => posts.data.length;
 const reducerName = "posts";
 const createActionName = name => `app/${reducerName}/${name}`;
 
-export const LOAD_POSTS = createActionName("LOAD_POSTS");
 export const START_REQUEST = createActionName("START_REQUEST");
 export const END_REQUEST = createActionName("END_REQUEST");
+export const ERROR_REQUEST = createActionName("ERROR_REQUEST");
+export const LOAD_POSTS = createActionName("LOAD_POSTS");
 
-export const loadPosts = payload => ({ payload, type: LOAD_POSTS });
 export const startRequest = () => ({ type: START_REQUEST });
 export const endRequest = () => ({ type: END_REQUEST });
+export const errorRequest = error => ({ error, type: ERROR_REQUEST });
+export const loadPosts = payload => ({ payload, type: LOAD_POSTS });
 
 /* INITIAL STATE */
 const initialState = {
   data: [],
   request: {
-    pending: false
+    pending: false,
+    error: null,
+    success: null
   }
 };
 
@@ -33,9 +37,20 @@ const initialState = {
 export default function reducer(statePart = initialState, action = {}) {
   switch (action.type) {
     case START_REQUEST:
-      return { ...statePart, request: { pending: true } };
+      return {
+        ...statePart,
+        request: { pending: true, error: null, success: null }
+      };
     case END_REQUEST:
-      return { ...statePart, request: { pending: false } };
+      return {
+        ...statePart,
+        request: { pending: false, error: null, success: true }
+      };
+    case ERROR_REQUEST:
+      return {
+        ...statePart,
+        request: { pending: false, error: action.error, success: false }
+      };
     case LOAD_POSTS:
       return { ...statePart, data: action.payload };
     default:
@@ -53,7 +68,7 @@ export const loadPostsRequest = () => {
       dispatch(loadPosts(res.data));
       dispatch(endRequest());
     } catch (e) {
-      dispatch(endRequest());
+      dispatch(errorRequest(e.message));
     }
   };
 };
