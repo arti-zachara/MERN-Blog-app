edd5d3f597d7dfd83aa5074b57be5a295e8c6207
arti Zachara <magdalena.j.zachara@gmail.com>
2019-07-28 23:24:54 +0200

    Add single post reducer to PostsRedux

--- client/src/redux/PostsRedux.js
+++ client/src/redux/PostsRedux.js
@@ -6,6 +6,7 @@ import { API_URL } from "../config";
 export const getRequest = ({ posts }) => posts.request;
 export const getPosts = ({ posts }) => posts.data;
 export const getPostsNumber = ({ posts }) => posts.data.length;
+export const getSinglePost = ({ posts }) => posts.singlePost;
 
 /* ACTIONS */
 // action name creator
@@ -16,11 +17,13 @@ export const START_REQUEST = createActionName("START_REQUEST");
 export const END_REQUEST = createActionName("END_REQUEST");
 export const ERROR_REQUEST = createActionName("ERROR_REQUEST");
 export const LOAD_POSTS = createActionName("LOAD_POSTS");
+export const LOAD_SINGLE_POST = createActionName("LOAD_SINGLE_POST");
 
 export const startRequest = () => ({ type: START_REQUEST });
 export const endRequest = () => ({ type: END_REQUEST });
 export const errorRequest = error => ({ error, type: ERROR_REQUEST });
 export const loadPosts = payload => ({ payload, type: LOAD_POSTS });
+export const loadSinglePost = payload => ({ payload, type: LOAD_SINGLE_POST });
 
 /* INITIAL STATE */
 const initialState = {
@@ -29,6 +32,11 @@ const initialState = {
     pending: false,
     error: null,
     success: null
+  },
+  singlePost: {
+    id: "",
+    title: "",
+    content: ""
   }
 };
 
@@ -53,6 +61,8 @@ export default function reducer(statePart = initialState, action = {}) {
       };
     case LOAD_POSTS:
       return { ...statePart, data: action.payload };
+    case LOAD_SINGLE_POST:
+      return { ...statePart, singlePost: action.payload };
     default:
       return statePart;
   }
@@ -66,6 +76,21 @@ export const loadPostsRequest = () => {
       let res = await axios.get(`${API_URL}/posts`);
       await new Promise((resolve, reject) => setTimeout(resolve, 2000));
       dispatch(loadPosts(res.data));
+      dispatch(endRequest());
+    } catch (e) {
+      dispatch(errorRequest(e.message));
+    }
+  };
+};
+
+export const loadSinglePostRequest = id => {
+  return async dispatch => {
+    dispatch(startRequest());
+
+    try {
+      let res = await axios.get(`${API_URL}/posts/${id}`);
+      await new Promise((resolve, reject) => setTimeout(resolve, 2000));
+      dispatch(loadSinglePost(res.data));
       dispatch(endRequest());
     } catch (e) {
       dispatch(errorRequest(e.message));
