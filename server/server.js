df7b4ba1841c2417e8dc7420fc2a7d6b6283e692
arti Zachara <magdalena.j.zachara@gmail.com>
2019-07-28 21:27:25 +0200

    Add mongodb connection data

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 const config = require("./config");
 const postRoutes = require("./routes/post.routes");
@@ -10,6 +11,15 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use("/api", postRoutes);
+
+// connects our back end code with the database
+mongoose.connect(config.DB, {
+  useNewUrlParser: true
+});
+let db = mongoose.connection;
+
+db.once("open", () => console.log("Connected to the database"));
+db.on("error", err => console.log("Error " + err));
 
 app.listen(config.PORT, function() {
   console.log("Server is running on Port:", config.PORT);
