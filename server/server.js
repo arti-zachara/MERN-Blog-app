f212bdcffe4bd7f0828b8cc535ef350590e8ab58
arti Zachara <magdalena.j.zachara@gmail.com>
2019-07-28 20:52:14 +0200

    Add routes and controller

--- server/server.js
+++ server/server.js
@@ -2,30 +2,14 @@ const express = require("express");
 const cors = require("cors");
 
 const config = require("./config");
+const postRoutes = require("./routes/post.routes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-app.get("/api/posts", (req, res) => {
-  const data = [
-    {
-      id: "1adfasf",
-      title: "Lorem Ipsum",
-      content:
-        "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit."
-    },
-    {
-      id: "2evxc34",
-      title: "Lorem Ipsum II",
-      content:
-        "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit."
-    }
-  ];
-  res.json(data);
-});
+app.use("/api", postRoutes);
 
 app.listen(config.PORT, function() {
   console.log("Server is running on Port:", config.PORT);
