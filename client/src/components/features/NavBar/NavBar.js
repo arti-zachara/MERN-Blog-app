3a6cdb1117c402d707c4094555fd1f14984966b2
arti Zachara <magdalena.j.zachara@gmail.com>
2019-07-29 22:29:14 +0200

    Import Logo and MainMenu to NavBar

--- client/src/components/features/NavBar/NavBar.js
+++ client/src/components/features/NavBar/NavBar.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import Logo from "../../common/Logo/Logo";
+import MainMenu from "../../layout/MainMenu/MainMenu";
+
 class NavBar extends React.Component {
   render() {
     return (
       <div>
+        <Logo />
         <Link to="/">Home</Link>
         <Link to="/posts">Posts</Link>
         <Link to="/posts/new">Add Post</Link>
