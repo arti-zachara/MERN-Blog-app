5664ee6300087ab396480267f927fb2cd407abe2
arti Zachara <magdalena.j.zachara@gmail.com>
2019-07-29 23:08:31 +0200

    Reload TestData

--- server/testData.js
+++ server/testData.js
@@ -4,7 +4,7 @@ const loadTestData = async () => {
   const data = [
     {
       id: "21sd42sdsaaf",
-      title: "1How do I get funding for my startup?",
+      title: "How do I get funding for my startup?",
       content:
         " Getting funding for your startup can be a bit frustrating. You want <b>a lot of money</b> and <b>you don't have a lot to offer. But don't worry.</b> There is something you can do! I'll teach you everything you need to know. Are you ready?"
     },
@@ -15,6 +15,11 @@ const loadTestData = async () => {
         "Oh... It won't be so easy. I won't give you a list. <i>But... I'm going to give you some tips that will surely help you on the road.</i> So, let's get started!"
     }
   ];
+
+  // Post.deleteMany({ id: "21sd42sdsaaf" }, function(err) {
+  //   if (err) return handleError(err);
+  //   // deleted at most one tank document
+  // });
 
   try {
     let counter = await Post.countDocuments();
