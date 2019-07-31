983a006d5a162db9d6fa85f3c3e91b7bacb6b5cd
arti Zachara <magdalena.j.zachara@gmail.com>
2019-07-31 17:06:38 +0200

    Add PostFormContainer

--- client/src/components/pages/AddPost/AddPostPage.js
+++ client/src/components/pages/AddPost/AddPostPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import PostForm from "../../features/PostForm/PostForm";
+import PostForm from "../../features/PostForm/PostFormContainer";
 
 const AddPostPage = () => (
   <div>
