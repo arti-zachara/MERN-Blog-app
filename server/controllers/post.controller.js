2710cf9fb9213416b3de2674b2d4738b6fd9b532
arti Zachara <magdalena.j.zachara@gmail.com>
2019-07-31 15:31:02 +0200

    Add addPost route and controller with uuid

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -1,3 +1,4 @@
+const uuid = require("uuid");
 const Post = require("../models/post.model");
 
 // get all posts
@@ -9,10 +10,26 @@ exports.getPosts = async (req, res) => {
   }
 };
 
+// get a single post
 exports.getSinglePost = async (req, res) => {
   try {
     res.status(200).json(await Post.findOne({ id: req.params.id }));
   } catch (error) {
+    res.status(500).json(err);
+  }
+};
+
+// add new post
+exports.addPost = async function(req, res) {
+  try {
+    const { title, author, content } = req.body;
+
+    let newPost = new Post(req.body);
+    newPost.id = uuid();
+
+    postSaved = await newPost.save();
+    res.status(200).json(postSaved);
+  } catch (err) {
     res.status(500).json(err);
   }
 };
