27f0c17c78dd7c3e26cef1f4e94df293ff00901f
arti Zachara <magdalena.j.zachara@gmail.com>
2019-08-02 13:09:19 +0200

    Add empty randompost page

---
+++ client/src/components/pages/RandomPostPage/RandomPostPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import PageTitle from "../../common/PageTitle/PageTitle";
+import SinglePostContainer from "../../features/SinglePost/SinglePostContainer";
+
+const RandomPostPage = props => (
+  <div>
+    <PageTitle>Random Post</PageTitle>
+  </div>
+);
+
+export default RandomPostPage;
