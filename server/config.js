2e5ca22d193808898f298a4d0659eb4d34d3dd46
arti Zachara <magdalena.j.zachara@gmail.com>
2019-08-02 22:21:11 +0200

    Add connection to external database

--- server/config.js
+++ server/config.js
@@ -1,4 +1,5 @@
 module.exports = {
   PORT: 8000,
-  DB: "mongodb://localhost:27017/mernapp"
+  DB:
+    "mongodb+srv://kodilla-MERN-blog-user:WdX7UVJDNIAwTki0@cluster0-qfesp.mongodb.net/test?retryWrites=true&w=majority"
 };
