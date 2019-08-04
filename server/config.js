88e58bbeebb7a05b89809d71ef418d65a7be2124
arti Zachara <magdalena.j.zachara@gmail.com>
2019-08-04 21:40:12 +0200

    Change config to use process.env.DB

--- server/config.js
+++ server/config.js
@@ -1,5 +1,4 @@
 module.exports = {
   PORT: 8000,
-  DB:
-    "mongodb+srv://kodilla-MERN-blog-user:WdX7UVJDNIAwTki0@cluster0-qfesp.mongodb.net/test?retryWrites=true&w=majority"
+  DB: process.env.DB
 };
